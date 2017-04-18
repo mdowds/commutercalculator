541b323e933cb875dd95d01aee1865bfda816158
Matt Dowds <matthewd99@gmail.com>
2017-04-18 19:03:04 +0100

    Renamed project to commuter-calculator and moved api code to 'api' directory

--- api/ccapi.py
+++ api/ccapi.py
@@ -1,16 +1,14 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
-from result import Result
+from api.result import Result
 
 app = Flask(__name__)
-api = Api(app)
+ccapi = Api(app)
 
 
 class JourneysTo(Resource):
     def get(self, station):
-
         stub = { "results": [Result("King's Cross").__dict__] }
-
         return jsonify(stub)
 
 
@@ -18,8 +16,8 @@ class Hello(Resource):
     def get(self):
         return "Hello world"
 
-api.add_resource(Hello, '/')
-api.add_resource(JourneysTo, '/journeys/to/<string:station>')
+ccapi.add_resource(Hello, '/')
+ccapi.add_resource(JourneysTo, '/journeys/to/<string:station>')
 
 if __name__ == '__main__':
-     app.run(debug=True, host='0.0.0.0')
+     app.run(debug=True)
