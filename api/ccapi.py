d91ee1f76ac61c8358b73101ef9ae584b7843847
Matt Dowds <matthewd99@gmail.com>
2017-04-19 21:56:35 +0100

    Added more verbose 404 handler

--- api/ccapi.py
+++ api/ccapi.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from api.result import Result
 
@@ -16,8 +16,13 @@ class Hello(Resource):
     def get(self):
         return "Hello world"
 
-ccapi.add_resource(Hello, '/')
-ccapi.add_resource(JourneysTo, '/journeys/to/<string:station>')
+ccapi.add_resource(Hello, '/api')
+ccapi.add_resource(JourneysTo, '/api/journeys/to/<string:station>')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This route does not exist {}'.format(request.url), 404
 
 if __name__ == '__main__':
-     app.run(debug=True)
+    #app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
