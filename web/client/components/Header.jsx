52ce55836d0ac7a13438700463e9d265c2008d7e
Matt Dowds <matthewd99@gmail.com>
2017-05-06 18:08:13 +0100

    Converted Header and Map to component functions

--- web/client/components/Header.jsx
+++ web/client/components/Header.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 
-export default class Header extends React.Component {
-    render() {
+export default function Header(props) {
+    const divStyle = {
+        padding: "5px",
+        position: "fixed",
+        zIndex: 1,
+        backgroundColor: "white",
+        width: "100%"
+    };
 
-        const divStyle = {
-            padding: "5px",
-            position: "fixed",
-            zIndex: 1,
-            backgroundColor: "white",
-            width: "100%"
-        };
+    const h1Style = {
+        margin: "2px 0",
+        fontSize: "25px"
+    };
 
-        const h1Style = {
-            margin: "2px 0",
-            fontSize: "25px"
-        };
-
-        return (
-            <div style={divStyle}>
-                <h1 style={h1Style}>Commuter Calculator</h1>
-                {this.props.children}
-            </div>
-        );
-    }
+    return (
+        <div style={divStyle}>
+            <h1 style={h1Style}>Commuter Calculator</h1>
+            {props.children}
+        </div>
+    );
 }
--- web/client/components/Map.jsx
+++ web/client/components/Map.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Map extends React.Component {
+export default function Map(props) {
+    const mapStyle = {
+        width: '100%',
+        height: '100%'
+    };
 
-    render() {
-        const mapStyle = {
-            width: '100%',
-            height: '100%'
-        };
-
-        return (
-            <div ref={this.props.divRef} style={mapStyle}>
-                Map loading
-                {this.props.children};
-            </div>
-        );
-    }
+    return (
+        <div ref={props.divRef} style={mapStyle}>
+            Map loading
+            {props.children};
+        </div>
+    );
 }
 
 Map.propTypes = {
