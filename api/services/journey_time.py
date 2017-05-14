5eebbb575d34012056ae6cacf4eeb3e12ba43b00
Matt Dowds <matthewd99@gmail.com>
2017-05-14 12:25:31 +0100

    Added _ to private function names in journey_time

--- api/services/journey_time.py
+++ api/services/journey_time.py
@@ -8,33 +8,33 @@ from typing import Union, Sequence
 
 @curried
 def get_journey_time(destination: Station, origin: Station) -> Union[int, None]:
-    return Option(time_from_db, origin, destination).or_call(get_time_and_save, origin, destination).value
+    return Option(_time_from_db, origin, destination).or_call(_get_time_and_save, origin, destination).value
 
 
-def time_from_db(origin: Station, destination: Station) -> Union[int, None]:
+def _time_from_db(origin: Station, destination: Station) -> Union[int, None]:
     times = JourneyTime.select().where((JourneyTime.origin == origin.sid) & (JourneyTime.destination == destination.sid))
-    return process_times(times)
+    return _process_times(times)
 
 
-def process_times(times: Sequence[JourneyTime]) -> Union[int, None]:
+def _process_times(times: Sequence[JourneyTime]) -> Union[int, None]:
     if len(times) == 0: return None
     # TODO: Replace with more sophisticated averaging
     return times[0].time
 
 
-def get_time_and_save(origin: Station, destination: Station) -> Union[int, None]:
-    pipe = F() >> time_from_gmaps(origin) >> bind(save_to_db(origin, destination))
+def _get_time_and_save(origin: Station, destination: Station) -> Union[int, None]:
+    pipe = F() >> _time_from_gmaps(origin) >> bind(_save_to_db(origin, destination))
     return pipe(destination).value
 
 
 @curried
-def time_from_gmaps(origin: Station, destination: Station) -> Maybe:
+def _time_from_gmaps(origin: Station, destination: Station) -> Maybe:
     pipe = F() >> gmaps.build_params(origin) >> gmaps.get_directions >> safe(gmaps.extract_duration) >> bind(secs_to_mins)
 
     return pipe(destination)
 
 
 @curried
-def save_to_db(origin: Station, destination: Station, time: int) -> Union[int, None]:
+def _save_to_db(origin: Station, destination: Station, time: int) -> Union[int, None]:
     JourneyTime.create(origin=origin.sid, destination=destination.sid, time=int(time))
     return time
--- api/tests/services/journey_time_tests.py
+++ api/tests/services/journey_time_tests.py
@@ -1,9 +1,9 @@
 import unittest
-from api.services.journey_time import *
+from api.services.journey_time import _process_times, JourneyTime
 
 
 class JourneyTimeTest(unittest.TestCase):
 
     def test_process_times(self):
-        self.assertEqual(2, process_times([JourneyTime(time=2)]))
-        self.assertEqual(None, process_times([]))
+        self.assertEqual(2, _process_times([JourneyTime(time=2)]))
+        self.assertEqual(None, _process_times([]))
