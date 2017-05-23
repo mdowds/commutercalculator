9d26949958d8f31a768cdd9073b17966252c04f1
Matt Dowds <matthewd99@gmail.com>
2017-05-23 16:17:12 +0100

    Update journey time script now updates all 3 stations

--- api/data/update/journey_time.py
+++ api/data/update/journey_time.py
@@ -5,7 +5,7 @@ from typing import Sequence
 import datetime
 
 
-def _update(destination: Station, all_stations: Sequence[Station]):
+def _update(all_stations: Sequence[Station], destination: Station):
     print("Updating " + destination.name)
     origins = filter_(lambda s: s.sid != destination.sid, all_stations)
     times = update_journey_times(destination, origins)
@@ -17,4 +17,4 @@ def _update(destination: Station, all_stations: Sequence[Station]):
 terminals_to_update = Station.select().where(Station.major_station == True).order_by(Station.journey_times_updated).limit(3)
 all_stations = Station.select().order_by(Station.name)
 
-_update(terminals_to_update[0], all_stations)
+for terminal in terminals_to_update: _update(all_stations, terminal)
