d8ed6df723706f81f1d54d4b161170c508638bc9
Matt Dowds <matthewd99@gmail.com>
2017-05-28 20:22:19 +0100

    Adjusted search form styles

--- web/client/components/SearchForm.jsx
+++ web/client/components/SearchForm.jsx
@@ -22,36 +22,36 @@ export default class SearchForm extends React.Component {
     }
 
     render() {
+        const borderStyle = "1px solid grey";
 
         const acMenuStyle = {
             borderRadius: '3px',
             boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
             background: 'rgba(255, 255, 255, 0.9)',
-            padding: '2px 0',
             fontSize: '90%',
             position: 'fixed',
-            overflow: 'auto',
-            maxHeight: '50%',
-            zIndex: 10
+            overflow: 'hidden',
+            maxHeight: 400,
+            zIndex: 10,
+            border: borderStyle,
         };
 
         return (
             <div style={{padding: 5, boxSizing: "border-box"}}>
-                <div>Journeys to
-                    <Autocomplete
-                        value={this.state.inputText}
-                        items={this.props.destinations ? this.props.destinations : []}
-                        getItemValue={station => station.name}
-                        renderItem={(station, isHighlighted) => (
-                            <div style={{zIndex: 10}} key={station.id}>{station.name}</div>
-                        )}
-                        menuStyle={acMenuStyle}
-                        onChange={(event, value) => this.setState({inputText: value})}
-                        onSelect={(value, station) => this.setState({inputText: station.name, selectedStation: station})}
-                        shouldItemRender={this.suggestStationName}
-                    />
-                </div>
-                <div><input id="submitSearch" type="button" value="Search" onClick={this.handleSubmit}/></div>
+                <span style={{fontSize: "90%", paddingRight: 5}}>Journeys to</span>
+                <Autocomplete
+                    value={this.state.inputText}
+                    items={this.props.destinations ? this.props.destinations : []}
+                    getItemValue={station => station.name}
+                    renderItem={(station, isHighlighted) => (
+                        <div style={{zIndex: 10, padding: 10, borderBottom: borderStyle}} key={station.id}>{station.name}</div>
+                    )}
+                    menuStyle={acMenuStyle}
+                    onChange={(event, value) => this.setState({inputText: value})}
+                    onSelect={(value, station) => this.setState({inputText: station.name, selectedStation: station})}
+                    shouldItemRender={this.suggestStationName}
+                />
+                <input id="submitSearch" type="button" value="Go" onClick={this.handleSubmit}/>
             </div>
         );
     }
