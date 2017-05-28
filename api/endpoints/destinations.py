f66db99f65f9cfc357814ddd0d38cfb559f4653a
Matt Dowds <matthewd99@gmail.com>
2017-05-28 17:12:21 +0100

    Added destinations endpoint

---
+++ api/endpoints/destinations.py
@@ -0,0 +1,15 @@
+from flask import jsonify
+from flask_restful import Resource
+from flask_restful.utils import cors
+from api.data import Station, serialize_station
+from api.lib.functional import F
+from api.utils import map_
+
+
+class Destinations(Resource):
+
+    @cors.crossdomain(origin='*')
+    def get(self):
+        destinations = Station.select().where(Station.major_station == True)
+        output_pipe =  F() >> map_(serialize_station) >> jsonify
+        return output_pipe(destinations)
