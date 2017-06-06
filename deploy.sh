63e5fd73919528151cb522950ec00aaf5479dc87
Matt Dowds <matthewd99@gmail.com>
2017-06-06 18:24:20 +0100

    Added automated deployment config

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+eval "$(ssh-agent -s)"
+echo "$DEPLOY_KEY" > deploy_key.pem
+chmod 600 deploy_key.pem
+ssh-add deploy_key.pem
+git remote add deploy $REPO_URI
+git push deploy master
