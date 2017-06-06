f11641c4cf88f022958912b9cc7bd0a6cfef1448
Matt Dowds <matthewd99@gmail.com>
2017-06-06 20:54:06 +0100

    Disabled broken deploy script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#rm -f .coverage coverage.json web/package-lock.json
-eval "$(ssh-agent -s)"
-echo "$DEPLOY_KEY" > deploy_key.pem
-chmod 600 deploy_key.pem
-ssh-add deploy_key.pem
-git remote add deploy $REPO_URI
-git push deploy master
