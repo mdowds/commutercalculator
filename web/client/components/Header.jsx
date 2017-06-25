399d07a7ae22bbe6b0326552eeca87a6b4b88f45
Matt Dowds <matthewd99@gmail.com>
2017-06-25 14:22:09 +0100

    Updated Header component styles

--- web/client/components/Header.jsx
+++ web/client/components/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Header(props) {
     const divStyle = {
         backgroundColor: "white",
-        width: "100%",
-        height: 90
+        width: "100%"
     };
 
     const h1Style = {
         margin: "2px 0",
-        fontSize: "25px"
+        fontSize: "25px",
+        textAlign: "center"
     };
 
     return (
