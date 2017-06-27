4a29379aa0af978140634aaf8f76790ac7d1cf2e
Matt Dowds <matthewd99@gmail.com>
2017-06-27 18:58:07 +0100

    Reworked journey time updater to be run from shell script

--- api/data/update/journey_time.py
+++ api/data/update/journey_time.py
@@ -1,29 +1,35 @@
 import datetime
+import sys
 from typing import Sequence
+from collections import namedtuple
 
 from api.data import Station, JourneyTime
-from api.lib.functional import curried
+from api.lib.functional import curried, F
 from api.lib.utils import filter_, map_
 from api.services.journey_time import update_journey_time
 
+Output = namedtuple('Output', ('destination', 'times'))
+
 
 def _update(all_stations: Sequence[Station], destination: Station):
-    print("Updating " + destination.name)
     origins = filter_(lambda s: s.sid != destination.sid, all_stations)
     times = map_(_update_origin(destination), origins)
-    print(str(len(times)) + " times inserted")
+    # times = [_update_origin(destination, origins[0])]
 
     if len(times) > 0:
         destination.journey_times_updated = datetime.datetime.now()
         destination.save()
 
+    return Output(destination.name, str(len(times)))
+
 
 @curried
 def _update_origin(destination: Station, origin: Station) -> JourneyTime:
-    print("Getting time for " + origin.name + " to " + destination.name)
+    # print("Getting time for " + origin.name + " to " + destination.name)
     update = update_journey_time(destination, origin)
     if update.get_error():
         print("Error: " + update.get_error())
+        sys.exit(1)
 
     return update.get_value()
 
@@ -35,4 +41,10 @@ stations_to_update = Station.select()\
 
 all_stations = Station.select().order_by(Station.name)
 
-for station in stations_to_update: _update(all_stations, station)
+join = lambda x: ", ".join(x)
+update_pipe = F() >> \
+              map_(lambda station: _update(all_stations, station)) >> \
+              map_(lambda update: update.destination + " (" + update.times + ")") >> \
+              join
+
+print("Updated journey times for " + update_pipe(stations_to_update[0]))
