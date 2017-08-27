0b16d3426b3cac5092503085d1436e9bae93e05e
Matt Dowds <matthewd99@gmail.com>
2017-08-27 15:24:52 +0100

    Converted Result component to class

--- web/client/components/Result.tsx
+++ web/client/components/Result.tsx
@@ -8,48 +8,55 @@ interface ResultProps {
     onSelectResult(selectedResult: JourneyResult): void;
 }
 
-function travelcardDescription(travelcard: Travelcard) {
-    return <div>Zone {travelcard.minZone} to {travelcard.maxZone} travelcard: £{travelcard.price}</div>
+interface ResultState {
+
 }
 
-function makeDetails(result: JourneyResult) {
-    const travelcardDiv = result.seasonTickets.travelcard ? travelcardDescription(result.seasonTickets.travelcard) : null;
-    const seasonTicketDiv = result.seasonTickets.seasonTicket ? <div>Direct season ticket: £{result.seasonTickets.seasonTicket.price}</div> : null;
+export default class Result extends React.Component<ResultProps, ResultState> {
 
-    const seasonTicketsDescription = seasonTicketsHasValues(result.seasonTickets) ?
-        <div>{travelcardDiv}{seasonTicketDiv}</div> :
-        <div>Season ticket not found</div>;
+    travelcardDescription(travelcard: Travelcard) {
+        return <div>Zone {travelcard.minZone} to {travelcard.maxZone} travelcard: £{travelcard.price}</div>
+    }
 
-    const directionsLink = <span><a href={result.directionsUrl}>View directions on Google Maps</a> (May show a different journey time)</span>;
+    makeDetails(result: JourneyResult) {
+        const travelcardDiv = result.seasonTickets.travelcard ? this.travelcardDescription(result.seasonTickets.travelcard) : null;
+        const seasonTicketDiv = result.seasonTickets.seasonTicket ? <div>Direct season ticket: £{result.seasonTickets.seasonTicket.price}</div> : null;
 
-    return <div>{seasonTicketsDescription}{directionsLink}</div>;
-}
+        const seasonTicketsDescription = this.seasonTicketsHasValues(result.seasonTickets) ?
+            <div>{travelcardDiv}{seasonTicketDiv}</div> :
+            <div>Season ticket not found</div>;
 
-function seasonTicketsHasValues(seasonTickets: SeasonTickets) {
-    return seasonTickets.travelcard || seasonTickets.seasonTicket;
-}
+        const directionsLink = <span><a href={result.directionsUrl}>View directions on Google Maps</a> (May show a different journey time)</span>;
 
-export default function Result(props: ResultProps) {
-    const bgColour = props.isSelected ? '#ddd' : 'white';
+        return <div>{seasonTicketsDescription}{directionsLink}</div>;
+    }
 
-    const divStyle: CSSProperties = {
-        padding: 10,
-        borderBottom: "1px solid grey",
-        backgroundColor: bgColour
-    };
+    seasonTicketsHasValues(seasonTickets: SeasonTickets) {
+        return seasonTickets.travelcard || seasonTickets.seasonTicket;
+    }
 
-    const details = props.isSelected ? <div style={{margin: '10px 0px 0px 30px', fontSize: '75%'}}>{makeDetails(props.result)}</div> : null;
+    render() {
+        const bgColour = this.props.isSelected ? '#ddd' : 'white';
 
-    const icon = props.isSelected ? "img/chevron-up.svg" : "img/chevron-down.svg";
+        const divStyle: CSSProperties = {
+            padding: 10,
+            borderBottom: "1px solid grey",
+            backgroundColor: bgColour
+        };
 
-    return (
-        <div style={divStyle} onClick={() => {props.onSelectResult(props.result)}}>
-            <div>
-                <div style={{float: "left", marginRight: 10}}><img src={icon} style={{width: 20, height: 20}}/></div>
-                <div style={{display: "inline-block", width: "65%"}}>{props.result.origin.name}</div>
-                <div style={{float: "right"}}>{props.result.journeyTime} mins</div>
+        const details = this.props.isSelected ? <div style={{margin: '10px 0px 0px 30px', fontSize: '75%'}}>{this.makeDetails(this.props.result)}</div> : null;
+
+        const icon = this.props.isSelected ? "img/chevron-up.svg" : "img/chevron-down.svg";
+
+        return (
+            <div style={divStyle} onClick={() => {this.props.onSelectResult(this.props.result)}}>
+                <div>
+                    <div style={{float: "left", marginRight: 10}}><img src={icon} style={{width: 20, height: 20}}/></div>
+                    <div style={{display: "inline-block", width: "65%"}}>{this.props.result.origin.name}</div>
+                    <div style={{float: "right"}}>{this.props.result.journeyTime} mins</div>
+                </div>
+                {details}
             </div>
-            {details}
-        </div>
-    );
+        );
+    }
 }
