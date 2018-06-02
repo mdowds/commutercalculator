7bc247550f136c5f0e34f411b868f9e5949e1ec4
Matt Dowds <matthewd99@gmail.com>
2018-06-02 22:12:29 +0100

    Update destination endpoint tests to work with new version of peewee

--- api/tests/destinations/endpoint_tests.py
+++ api/tests/destinations/endpoint_tests.py
@@ -1,28 +1,12 @@
-import unittest
-
 from peewee import SqliteDatabase
-from playhouse.test_utils import test_database
 
-import api.tests.helpers as helpers
-from api.destinations.endpoint import *
 from api.destinations.endpoint import _get_destinations
+from api.tests.dbtestcase import DBTestCase
 
 test_db = SqliteDatabase(':memory:')
 
 
-class DestinationsTests(unittest.TestCase):
-
-    def setUp(self):
-        self._all_stations = helpers.create_station_test_data()
-        for station in self._all_stations: station.save(force_insert=True)
-
-    def tearDown(self):
-        Station.delete()
-
-    def run(self, result=None):
-        # All queries will be run in `test_db`
-        with test_database(test_db, [Station]):
-            super(DestinationsTests, self).run(result)
+class DestinationsTests(DBTestCase):
 
     def test_get_destinations_filters_zone(self):
         self.assertEqual(2, len(_get_destinations()))
