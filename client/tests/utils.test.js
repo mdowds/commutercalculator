029f76a2ebb9633eb9a3618fc3ccf814655e9d7a
Matt Dowds <matthewd99@gmail.com>
2019-01-11 17:46:25 +0000

    Don't track transpiled js files in git

--- client/tests/utils.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var utils_1 = require("../utils");
-test("isEmptyObject() returns correct results", function () {
-    expect(utils_1.isEmptyObject({})).toEqual(true);
-    expect(utils_1.isEmptyObject({ foo: "bar" })).toEqual(false);
-});
